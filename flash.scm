d818bb0e7e1d59dbad6792769243c4cc491e20fb
Artem Shubovych <shybovycha@gmail.com>
2025-10-28 20:42:29 +1100

    Refactor code

--- flash.scm
+++ flash.scm
@@ -36,40 +36,53 @@
   (let* ([input (hash-ref *flash-state* 'input)])
     (set-status! (to-string "flash:" input))))
 
-(define (flash-render state rect frame)
-  (let*
-    ([cursor-pos 
-       (if (and (list? (current-cursor)) (not (empty? (current-cursor))) (Position? (first (current-cursor))))
+(define (flash-first-cursor-pos)
+  (if (and (list? (current-cursor)) (not (empty? (current-cursor))) (Position? (first (current-cursor))))
            (first (current-cursor))
-           #f)]
-     [frame-rect
-       (if (Rect? (editor-focused-buffer-area))
+           #f))
+
+(define (flash-frame-rect)
+  (if (Rect? (editor-focused-buffer-area))
          (editor-focused-buffer-area)
          ;(buffer-area frame)
-         #f)]
-     [input (hash-ref *flash-state* 'input)])
-  (begin
+         #f))
+
+(define (flash-render state rect frame)
+  (let*
+    ([cursor-pos (flash-first-cursor-pos)]
+     [frame-rect (flash-frame-rect)]
+     [input (hash-ref *flash-state* 'input)]
+     [x (+ (area-y rect) 1 (position-col cursor-pos))]
+     [y (+ (area-x rect) (position-row cursor-pos))]
+     [style (style-with-bold (theme-scope "ui.cursor.match"))])
     (flash-update-status)
-    (frame-set-string! frame (+ (area-y rect) 1 (position-col cursor-pos)) (+ (area-x rect) (position-row cursor-pos)) "?" (style-with-bold (theme-scope "ui.cursor.match")))
-    )))
+    (frame-set-string! frame x y "?" style)))
+
+(define (flash-remove-last-input-char)
+  (let*
+       ([input (hash-ref *flash-state* 'input)]
+        [input-len (string-length input)])
+       (when (> input-len 0)
+         (set! *flash-state* (hash-insert *flash-state* 'input (substring input 0 (- input-len 1)))))))
+
+(define (flash-append-input-char key-char)
+  (let*
+    ([input (hash-ref *flash-state* 'input)]
+     [new-input (apply string (append (string->list input) (list key-char)))])
+    (set! *flash-state* (hash-insert *flash-state* 'input new-input))))
 
 (define (flash-handle-event state event)
   (define key-char (key-event-char event))
   (cond
-    [(key-event-escape? event) (set-status! "") event-result/close]
+    [(key-event-escape? event)
+     (set-status! "")
+     event-result/close]
     [(key-event-backspace? event)
-     (let*
-       ([input (hash-ref *flash-state* 'input)]
-        [input-len (string-length input)]
-        [key-str (to-string key-char)])
-       (if (> input-len 0)
-         (begin
-           (set! *flash-state* (hash-insert *flash-state* 'input (substring input 0 (- input-len 1))))
-           event-result/consume)
-         event-result/consume))
-     (flash-update-status)]
+     (flash-remove-last-input-char)
+     (flash-update-status)
+     event-result/consume]
     [(char? key-char)
-      (set! *flash-state* (hash-insert *flash-state* 'input (apply string (append (string->list (hash-ref *flash-state* 'input)) (list key-char)))))
+      (flash-append-input-char key-char)
       (flash-update-status)
       event-result/consume]
     [else
@@ -79,7 +92,14 @@
 (define (flash-handle-cursor-event state event) #f)
 
 (define (flash)
-  (push-component! (new-component! "flash" *flash-state* flash-render (hash "handle_event" flash-handle-event "cursor" flash-handle-cursor-event))))
+  (push-component!
+    (new-component!
+      "flash"
+      *flash-state*
+      flash-render
+      (hash
+        "handle_event" flash-handle-event
+        "cursor" flash-handle-cursor-event))))
 
 (provide flash)
 
